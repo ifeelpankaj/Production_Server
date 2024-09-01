3d42a74882c4fc2bd0ea5612ddd93b342a85fb9a
ifeelpankaj <ifeelpankaj@gmail.com>
2024-09-01 16:00:46 +0530

    Initial: Prettier Set Up

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-// import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config({
     languageOptions: {
@@ -10,7 +10,7 @@ export default tseslint.config({
         }
     },
     files: ['**/*.ts'],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked,eslintConfigPrettier],
     rules: {
         'no-console': 'error',
         'no-useless-catch': 0,
